11829f0277e19c3e1ba492c70ba8b61db9d58848
Justice Abban <abbanjustice134@gmail.com>
2026-07-07 17:44:57 +0000

    removed the vercel.json file

--- yes-app/next.config.ts
+++ yes-app/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 module.exports = {
-  allowedDevOrigins: ["10.249.76.177"],
   images: {
     remotePatterns: [
       {
